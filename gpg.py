f6efec1280d5d22f7306c23b39f5e5f0f3973cf1
vagelim <vagelim@gmail.com>
2013-09-20 14:21:27 -0400

    none

--- gpg.py
+++ gpg.py
@@ -10,21 +10,33 @@ path = os.path.abspath(sys.argv[0])
 user = platform.node()
 comment = 'test key creation'
 email = 'none@test.com'
-directory =  '/.gpg'
+directory =  '.gpg'
+pubkey_file = None
 
 
 #####GnuPG Integration with HTTP Reverse Shell#####
 
 #Create GPG object to interface with, local working directory for keys being <working dir>/.gpg
-gpg = gnupg.GPG(gnupghome= path + director)
+gpg = gnupg.GPG(gnupghome= path + "/" + directory)
 
-#Set options for GPG key
-input_data = gpg.gen_key_input(key_type="RSA", key_length=2048 , name_real=user , name_comment=comment , name_email=email)
 
-#Generate Key
-key = gpg.gen_key(input_data)
+#Check to see if key exists
+if not os.path.exists(user + ".pub"):
 
-#Export keys to variable to be saved to text file for sharing with clients
-ascii_armored_public_keys = gpg.export_keys(str(key))
+	#Set options for GPG key
+	input_data = gpg.gen_key_input(key_type="RSA", key_length=2048 , name_real=user , name_comment=comment , name_email=email)
 
-#Open file and write pub key
+	#Generate Key
+	key = gpg.gen_key(input_data)
+
+	#Export keys to variable to be saved to text file for sharing with clients
+	ascii_armored_public_keys = gpg.export_keys(str(key))
+
+	#Open file and write pub key
+	with  open(user+".pub", 'w') as pubkey_file:
+		pubkey_file.write(ascii_armored_public_keys)
+
+	#Import Server PubKey
+	with open("server.pub", 'r') as server_pubkey_file:
+		server_pubkey_tainted = server_pubkey_file.read()
+		server_pubkey = gpg.import_keys(server_pubkey_tainted)
